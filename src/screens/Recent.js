fc3d9a17f11955350434f0a7d256a821ff710337
manishram <rammanish630@gmail.com>
2021-08-26 23:34:50 +0530

    api-integration-start

--- src/screens/Recent.js
+++ src/screens/Recent.js
@@ -1,8 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { StyleSheet, View, ScrollView, Text } from 'react-native'
 import MatchCard from '../components/MatchCard'
+import RequestApi from '../api/RequestApi'
 
 const Recent = ({ navigation }) => {
+    const [results, setResults] = useState([])
+    const recentMatch = async () => {
+        try {
+            const response = await RequestApi.get('matches/', {
+                params: {
+                    status: 2,
+                    format: 4,
+                    token: 'ec471071441bb2ac538a0ff901abd249',
+                },
+            })
+            setResults(response)
+        } catch (err) {
+            console.log(err)
+        }
+    }
     return (
         <ScrollView showsVerticalScrollIndicator={false}>
             <View style={styles.container}>
